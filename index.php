8549235f4169ed671daf05589bbbaa096400bad2
hajjamn <hajjamn@gmail.com>
2024-05-08 23:37:48 +0200

    ADD: main css

--- index.php
+++ index.php
@@ -8,14 +8,24 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+  <link rel="stylesheet" href="./style/style.css">
 </head>
 
 <body>
 
   <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 
-  <div class="container">
-    <div id="app">{{ message }}</div>
+  <div id="app">
+    <header class="bg-brand-darker">
+      <div class=" container py-4">
+        <div class="row align-items-center">
+          <div class="col">Logo</div>
+        </div>
+      </div>
+    </header>
+    <main class="flex-grow-1 bg-brand-light">
+
+    </main>
   </div>
 
   <script src="./js/app.js"></script>
--- server.php
+++ server.php
@@ -4,3 +4,8 @@ $recordsString = file_get_contents('dischi.json');
 $recordsArray = json_decode($recordsString, true);
 
 var_dump($recordsArray);
+
+$data = [
+  'results' => $recordsArray,
+  'succes' => true
+];
